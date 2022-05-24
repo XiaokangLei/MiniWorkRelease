aa9ad9744fa88608498f0e5b67a1f1fdf1c6f012
Xiaokang Lei <13281183@bjtu.edu.cn>
2022-05-24 15:00:16 +0800

    remove unused code

--- miniprogram/pages/my/home.js
+++ miniprogram/pages/my/home.js
@@ -3,7 +3,8 @@ const config = require('../../config/config.js')
 
 Page({
   data: {
-    scrollTop: 0
+    scrollTop: 0,
+    signBtnTxt: "每日签到",
   },
   tapToUrl(e) {
     wx.navigateTo({
@@ -27,6 +28,15 @@ Page({
       },
     });
   },
+  show() {
+    this.$showDialog({
+        content: '敬请期待~',
+        showCancel: false,
+        success: res => {
+            console.log(res);
+        }
+    });
+},
   // 监听用户滑动页面事件。
   onPageScroll(e) {
     // 注意：请只在需要的时候才在 page 中定义此方法，不要定义空方法。以减少不必要的事件派发对渲染层-逻辑层通信的影响。
